a6e8aa6b52fb0ef38192e1701c82d05c2730575e
EmiBuffet <emibuffet@gmail.com>
2020-08-11 23:20:26 -0300

    HU3
    Intento de deploy 2!

--- TuDistribuidora/settings.py
+++ TuDistribuidora/settings.py
@@ -133,5 +133,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+import django_heroku
+django_heroku.settings(locals())
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
