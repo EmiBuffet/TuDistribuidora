a7ecce5d2ebee52226500e23573a273d65d5eb1e
EmiBuffet <emibuffet@gmail.com>
2020-08-11 22:34:30 -0300

    HU2
    Configuración local para Postgres 12.0

--- TuDistribuidora/settings.py
+++ TuDistribuidora/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'TuDistribuidora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TuDistribuidora',
+        'USER': 'postgres',
+        'PASSWORD': 'sardina20',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
