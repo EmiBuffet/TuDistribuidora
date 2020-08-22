8b2c95656a4e2b298f77ae56a62744e128a89f75
EmiBuffet <emibuffet@gmail.com>
2020-08-21 23:05:17 -0300

    HU1
    Tablas principales

--- distribuidora/admin.py
+++ distribuidora/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from distribuidora.models import Pedidos
+from distribuidora.models import Pedido
+from distribuidora.models import Categoria
+from distribuidora.models import Producto
+from distribuidora.models import DetallePedido
 
-admin.site.register(Pedidos)
+admin.site.register(Pedido)
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(DetallePedido)
--- distribuidora/models.py
+++ distribuidora/models.py
@@ -1,8 +1,10 @@
+from decimal import Decimal
+
 from django.contrib.auth.models import User
 from django.db import models
 
 
-class Pedidos(models.Model):
+class Pedido(models.Model):
     ESTADOS = (
         ('1', 'Solicitado'),
         ('2', 'Confirmado'),
@@ -11,10 +13,48 @@ class Pedidos(models.Model):
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     fecha_pedido = models.DateField()
-    fecha_entrega = models.DateField()
+    fecha_entrega = models.DateField(null=True, blank=True)
     direccion = models.CharField(max_length=100)
-    entregado = models.BooleanField()
+    entregado = models.BooleanField(null=True, blank=True)
     estado = models.CharField(max_length=1, choices=ESTADOS)
 
     class Meta:
         ordering = ['-fecha_pedido']
+
+    def __str__(self):
+        return u"%s %s %s" % (self.fecha_pedido, self.direccion, self.user)
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=150, null=True, blank=True)
+
+    class Meta:
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    codigo_producto = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=150, null=True, blank=True)
+    precio_unitario = models.DecimalField(max_digits=20, decimal_places=2, default=Decimal('0.00'))
+    imagen = models.CharField(null=True, blank=True, max_length=400)
+    stock = models.IntegerField(null=True, blank=True)
+    cantidad_unidades = models.IntegerField(null=True, blank=True)
+    fecha_baja = models.DateField(null=True, blank=True)
+    fecha_carga = models.DateField()
+
+    def __str__(self):
+        return u"%s %s %s" % (self.codigo_producto, self.descripcion, self.precio_unitario)
+
+
+class DetallePedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    precio_detalle = models.DecimalField(max_digits=20, decimal_places=2, default=Decimal('0.00'))
+    descuento = models.IntegerField(null=True, blank=True)
