e08e3507b5bc26767daefb47abef1766e0030229
EmiBuffet <emibuffet@gmail.com>
2020-09-09 20:46:37 -0300

    Agrego rest_framework Api para registrar con JWT

--- TuDistribuidora/settings.py
+++ TuDistribuidora/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'jwtauth',
     'distribuidora.apps.DistribuidoraConfig',
 ]
 
@@ -71,6 +73,19 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 WSGI_APPLICATION = 'TuDistribuidora.wsgi.application'
 
