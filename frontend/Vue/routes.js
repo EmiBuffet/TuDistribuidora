e7599b06e95e6be53bdf98978e26195e8a31d920
lucasdavidferrero@gmail.com <lucasdavidferrero@gmail.com>
2020-10-10 10:57:45 -0300

    Elimino archivos del frontend

--- frontend/Vue/routes.js
+++
@@ -1,7 +0,0 @@
-import Home from './components/home/Home.vue';
-import Login from './components/auth/Login.vue'
-
-export const routes = [
-    { path: '/' , component: Home },
-    { path: '/login' , component: Login }
-]
