043a90f052387b5b8f384501ac4733b962716626
EmiBuffet <emibuffet@gmail.com>
2020-11-07 19:12:00 -0300

    Agrego filtros en api productos y actualizar datos de usuario

--- TuDistribuidora/settings.py
+++ TuDistribuidora/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'jwtauth',
     'distribuidora.apps.DistribuidoraConfig',
     'corsheaders'
@@ -87,7 +88,11 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
+
 
 WSGI_APPLICATION = 'TuDistribuidora.wsgi.application'
 
--- distribuidora/views.py
+++ distribuidora/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import action
+import django_filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, filters
 from .models import Categoria, Producto, Usuario
 from .serializers import CategoriaSerializer, ProductoSerializer, UsuarioSerializer
 
@@ -20,10 +21,13 @@ class HomeView(APIView):
         return Response(content)
 
 
+
 class CategoriaViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['nombre']
     #Agregar validacion para cuando quieren Eliminar, si tiene productos cargados con esta.
 
 
@@ -32,13 +36,29 @@ class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['nombre']
+    ordering_fields = ('nombre', 'fecha_carga')
+
 
 class UsuarioViewSet(viewsets.ViewSet):
 
     def retrieve(self, request, pk=None):
         queryset = Usuario.objects.filter(username=pk)
-        contact = get_object_or_404(queryset, pk=1)
-        serializer = UsuarioSerializer(contact)
+        usuario = get_object_or_404(queryset, pk=1)
+        serializer = UsuarioSerializer(usuario)
         return Response(serializer.data)
+
+
+    def update(self, request, pk=None):
+        queryset = Usuario.objects.filter(username=pk)
+        usuario = get_object_or_404(queryset, pk=1)
+        serializer = UsuarioSerializer(usuario, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
